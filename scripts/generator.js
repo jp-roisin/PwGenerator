638d1eb4220c541d00c783877d5eaa78c0d2e115
jp-roisin <jeanpaul.roisin@protonmail.com>
2021-10-09 12:59:05 +0200

    generator done

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,71 +1,82 @@
 // Select DOM elements
 let password = document.querySelector('#password');
 let refreshPw = document.querySelector('#reload');
-let caps = document.querySelector('#caps');
-let numbers = document.querySelector('#numbers');
-let symbols = document.querySelector('#symbols');
+let capsCheckbox = document.querySelector('#caps');
+let numbersCheckbox = document.querySelector('#numbers');
+let symbolsCheckbox = document.querySelector('#symbols');
 let passwordLength = document.querySelector('#passwordLength');
 
+// Checkbox Var
+var caps = false;
+var numbers = false;
+var symbols = false;
+
 // Listen password RANGE
-var legth = 12;
 passwordLength.addEventListener('change', () => {
     length = passwordLength.value;
-    generatePassword(length);
+    generatePassword(length, caps, numbers, symbols);
     return length;
 })
 
+// Listen all 3 checkbox
+capsCheckbox.addEventListener('change', () =>{
+    if (capsCheckbox.checked){
+        caps = true;
+        generatePassword(length, caps, numbers, symbols);
+    }
+    else{
+        caps = false;
+        generatePassword(length, caps, numbers, symbols);
+    }
+});
+
+numbersCheckbox.addEventListener('change', () =>{
+    if (numbersCheckbox.checked){
+        numbers = true;
+        generatePassword(length, caps, numbers, symbols);
+    }
+    else{
+        numbers = false;
+        generatePassword(length, caps, numbers, symbols);
+    }
+});
+
+symbolsCheckbox.addEventListener('change', () =>{
+    if (symbolsCheckbox.checked){
+        symbols = true;
+        generatePassword(length, caps, numbers, symbols);
+    }
+    else{
+        symbols = false;
+        generatePassword(length, caps, numbers, symbols);
+    }
+});
 
 // Make the Button refresh the PW
 refreshPw.addEventListener('click', () => {
-    console.log(length);
-    generatePassword(length);
+    generatePassword(length, caps, numbers, symbols);
 });
 
-
-function generatePassword(length) {
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+// FUNCTION GENERATE PASSWORD
+function generatePassword(length, caps, numbers, symbols) {
+    let charset = "abcdefghijklmnopqrstuvwxyz",
         retVal = "";
+
+    if (!length || length == 0){
+        length = 12;
+    }
+    if (caps){
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (numbers){
+        charset += "0123456789";
+    }
+    if (symbols){
+        charset += "!#$%&'()*+,-./:;<=>?@[\\]\"^_`{|}~";
+    }
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
     password.value = retVal;
     return retVal;
 }
-
-
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
-// ------------------------------------------------------------------------
-
-
-
-// Listen all checkbox
-caps.addEventListener('change', () =>{
-    if (caps.checked){
-        console.log('Caps checked');
-    }
-    else{
-        console.log('Caps unchecked');
-    }
-});
-
-numbers.addEventListener('change', () =>{
-    if (numbers.checked){
-        console.log('numbers checked');
-    }
-    else{
-        console.log('numbers unchecked');
-    }
-});
-
-symbols.addEventListener('change', () =>{
-    if (symbols.checked){
-        console.log('symbols checked');
-    }
-    else{
-        console.log('symbols unchecked');
-    }
-});
